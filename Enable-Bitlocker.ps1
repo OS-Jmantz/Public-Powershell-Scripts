bef9c197e6f955b276397b1203c888c2a450a8a2
Jimmy Mantz <105438249+OS-Jmantz@users.noreply.github.com>
2023-04-27 15:48:03 -0400

    Update Enable-Bitlocker.ps1

--- Enable-Bitlocker.ps1
+++ Enable-Bitlocker.ps1
@@ -1,13 +1,13 @@
-$BitLocker_Status=(Get-BitLockerVolume -MountPoint "C:").VolumeStatus
+$BitLocker_Status=(Get-BitLockerVolume -MountPoint "C:")
 
-if ($BitLocker_Status -eq "FullyDecrypted") {
+if ($BitLocker_Status.VolumeStatus -eq "FullyDecrypted") {
     Write-Host "Bitlocker is not enabled, enabling now..."
     Enable-BitLocker -MountPoint "C:" -EncryptionMethod XtsAes256 -TpmProtector -UsedSpaceOnly -SkipHardwareTest
     Write-host "Bitlocker Encryption currently in progress."
 }
 
-if ($BitLocker_Status -eq "EncryptionInProgress"){
-    Write-Host "Encryption is currently in progress, drive is $((Get-BitlockerVolume -MountPoint "C:").EncryptionPercentage)% encrypted"
+if ($BitLocker_Status.VolumeStatus -eq "EncryptionInProgress"){
+    Write-Host "Encryption is currently in progress, drive is $($Bitlocker_Status.EncryptionPercentage)% encrypted"
 }   else {
     Write-Host "Bitlocker is already enabled and drive is fully encrypted."
 }
