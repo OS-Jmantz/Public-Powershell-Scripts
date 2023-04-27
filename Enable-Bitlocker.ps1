53e351cec62b239282cbd23f42e624bcf7915fbb
Jimmy Mantz <105438249+OS-Jmantz@users.noreply.github.com>
2023-04-27 15:51:12 -0400

    Update Enable-Bitlocker.ps1
    Nested if statement.

--- Enable-Bitlocker.ps1
+++ Enable-Bitlocker.ps1
@@ -4,10 +4,8 @@ if ($BitLocker_Status.VolumeStatus -eq "FullyDecrypted") {
     Write-Host "Bitlocker is not enabled, enabling now..."
     Enable-BitLocker -MountPoint "C:" -EncryptionMethod XtsAes256 -TpmProtector -UsedSpaceOnly -SkipHardwareTest
     Write-host "Bitlocker Encryption currently in progress."
-}
-
-if ($BitLocker_Status.VolumeStatus -eq "EncryptionInProgress"){
+} if ($BitLocker_Status.VolumeStatus -eq "EncryptionInProgress"){
     Write-Host "Encryption is currently in progress, drive is $($Bitlocker_Status.EncryptionPercentage)% encrypted"
-}   else {
+ }   else {
     Write-Host "Bitlocker is already enabled and drive is fully encrypted."
-}
+ }
